bd51acd46a450a0aed3c97ec2ebe8f1dc972a28c
yujieyu7 <448994814@qq.com>
2015-08-02 19:41:00 +0800

    v1.2

--- whiteLS.js
+++ whiteLS.js
@@ -7,12 +7,12 @@
 
 //<script src="whiteLS.js"></script> 使用链接
 
-if (!window.localStorage.length) {//避免重复添加
+if (!window.localStorage.length || window.localStorage.length == 2) {//避免重复添加
 
-	var name1 = ['微博','虎扑','知乎','新浪','果壳','腾讯','天猫','Quora','好123','维基百科','QQ邮箱','淘宝','京东','亚马逊','印象笔记','有道云','前端网','拉勾','博客园','GitHub','segmentfault','FireFox','Html5','YouTube','V2EX'];
+	var name1 = ['微博','虎扑','知乎','新浪','果壳','腾讯','天猫','Quora','好123','维基百科','QQ邮箱','淘宝','京东','亚马逊','印象笔记','有道云','前端网','拉勾','博客园','GitHub','segmentfault','FireFox','Html5','YouTube','V2EX','百度云','stackoverflow'];
 
 	var link = ['http://weibo.com/u/2562606833/home?wvr=5&sudaref=passport.weibo.com',
-		'http://www.hupu.com/','http://www.zhihu.com/','http://www.sina.com.cn/','http://www.guokr.com/','http://www.qq.com/','https://www.tmall.com/','http://www.quora.com/','http://www.hao123.com/?src=skycn_xzq','https://zh.wikipedia.org/wiki/Wikipedia:%E9%A6%96%E9%A1%B5','http://mail.qq.com/cgi-bin/frame_html?sid=6RfW9ejfEdJ5T1UA&r=0e74d04fccb4b42da4ad79a28375c430','https://www.taobao.com/','http://www.jd.com/','http://www.amazon.cn/?tag=baiduiclickcn-23&ref=pz_ic_bd_u_0328pp24','https://app.yinxiang.com/Home.action#n=ee349c91-d6bf-40fa-a7cc-428d23db6271&ses=4&sh=2&sds=5&','http://note.youdao.com/web/list?notebook=%2F&sortMode=0&note=%2FSVR018730A41A884ACF812BAEB401946485%2Fweb1431446437572','http://www.w3cfuns.com/portal.php','http://www.lagou.com/','http://www.cnblogs.com/','https://github.com/','http://segmentfault.com/','https://developer.mozilla.org/zh-CN/','http://www.html5cn.org/','https://www.youtube.com/','http://www.v2ex.com/'
+		'http://www.hupu.com/','http://www.zhihu.com/','http://www.sina.com.cn/','http://www.guokr.com/','http://www.qq.com/','https://www.tmall.com/','http://www.quora.com/','http://www.hao123.com/?src=skycn_xzq','https://zh.wikipedia.org/wiki/Wikipedia:%E9%A6%96%E9%A1%B5','http://mail.qq.com/cgi-bin/frame_html?sid=6RfW9ejfEdJ5T1UA&r=0e74d04fccb4b42da4ad79a28375c430','https://www.taobao.com/','http://www.jd.com/','http://www.amazon.cn/?tag=baiduiclickcn-23&ref=pz_ic_bd_u_0328pp24','https://app.yinxiang.com/Home.action#n=ee349c91-d6bf-40fa-a7cc-428d23db6271&ses=4&sh=2&sds=5&','http://note.youdao.com/web/list?notebook=%2F&sortMode=0&note=%2FSVR018730A41A884ACF812BAEB401946485%2Fweb1431446437572','http://www.w3cfuns.com/portal.php','http://www.lagou.com/','http://www.cnblogs.com/','https://github.com/','http://segmentfault.com/','https://developer.mozilla.org/zh-CN/','http://www.html5cn.org/','https://www.youtube.com/','http://www.v2ex.com/','http://pan.baidu.com/disk/home#from=share_pan_logo&render-type=list-view','http://stackoverflow.com/'
 	];
 
 	var src = [];
